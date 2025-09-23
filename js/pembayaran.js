f8bd5b308f1fc7488244938cfaf1140d082b8792
maiillll <adityauzmanchandra@gmail.com>
2025-09-24 00:43:03 +0700

    Update pembayaran.js

--- js/pembayaran.js
+++ js/pembayaran.js
@@ -1,26 +1,12 @@
 $(document).ready(function() {
     const table = $('#dataTable').DataTable({"language": {"url": "//cdn.datatables.net/plug-ins/1.10.25/i18n/Indonesian.json"}});
-
     db.collection('bills').orderBy('dibuatPada', 'desc').onSnapshot(snapshot => {
         table.clear().draw();
         snapshot.forEach((doc, index) => {
             const bill = doc.data();
-            let statusBadge = '';
-            if (bill.status === 'PENDING') {
-                statusBadge = `<span class="badge bg-warning text-dark">${bill.status}</span>`;
-            } else if (bill.status === 'PAID') {
-                statusBadge = `<span class="badge bg-success">${bill.status}</span>`;
-            } else {
-                 statusBadge = `<span class="badge bg-danger">${bill.status}</span>`;
-            }
-
+            let statusBadge = bill.status === 'PENDING' ? `<span class="badge bg-warning text-dark">${bill.status}</span>` : `<span class="badge bg-success">${bill.status}</span>`;
             table.row.add([
-                index + 1,
-                bill.tanggal,
-                doc.id,
-                bill.pasien.nama,
-                formatRupiah(bill.total),
-                statusBadge
+                index + 1, bill.tanggal, doc.id, bill.pasien.nama, formatRupiah(bill.total), statusBadge
             ]).draw(false);
         });
     });
