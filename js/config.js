02987aef3fb4075a0a787e6ef368aa59e8c4552d
maiillll <adityauzmanchandra@gmail.com>
2025-09-24 00:39:56 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Konfigurasi Firebase Anda (DIAMBIL DARI INFORMASI YANG ANDA BERIKAN)
 const firebaseConfig = {
   apiKey: "AIzaSyAr9-ZC-Po3e-dH3WPAmc7fEXZ7peEtSN8",
   authDomain: "klinik-26f61.firebaseapp.com",
@@ -13,5 +8,6 @@ const firebaseConfig = {
   appId: "1:388091198852:web:11401553ab9bd4b1bb71d1"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Inisialisasi Firebase dengan gaya yang kompatibel (v8)
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
