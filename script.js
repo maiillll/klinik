5305067b4aa475b2653a93e380bf87595433a0db
maiillll <adityauzmanchandra@gmail.com>
2025-09-23 15:06:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // Konfigurasi Firebase Anda (salin dari konsol Firebase)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyYOUR_API_KEY",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "1:YOUR_APP_ID:web:your-web-app-id"
+  apiKey: "AIzaSyAr9-ZC-Po3e-dH3WPAmc7fEXZ7peEtSN8",
+  authDomain: "klinik-26f61.firebaseapp.com",
+  projectId: "klinik-26f61",
+  storageBucket: "klinik-26f61.firebasestorage.app",
+  messagingSenderId: "388091198852",
+  appId: "1:388091198852:web:11401553ab9bd4b1bb71d1"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // Inisialisasi Firebase
 firebase.initializeApp(firebaseConfig);
